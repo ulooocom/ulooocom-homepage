56471d361f3404fd35430394fbe651674f3059fd
fusc <fusc@ulooo.com>
2022-07-07 21:04:30 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
-import thumbYouTube from '../public/images/links/youtube.png'
+import thumbYouTube from '../public/images/contents/magentoDevOnDokcerMacV22.jpg'
 import linksForRobinFu from '../public/images/works/linksforrobinfu.jpg'
 import Image from 'next/image'
 
