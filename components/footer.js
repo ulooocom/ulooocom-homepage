8c2bbdc844d0b29626e1a58ae2672bde36ada46f
fusc <fusc@ulooo.com>
2022-07-12 21:01:57 +0800

    + update

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      <p></p>&copy; {new Date().getFullYear()} Ulooocom. 保留所有权利. Thanks to <a href="https://www.craftz.dog/" >@craftzdog</a>
+      <p></p>&copy; {new Date().getFullYear()} Ulooocom. 保留所有权利, <a href="https://beian.miit.gov.cn/" target="_black">沪ICP备13025528号-4</a>. Thanks to <a href="https://www.craftz.dog/" >@craftzdog</a>
     </Box>
   )
 }
